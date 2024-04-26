0104bf9c423359961e0cc9d203837efb0ff54551
Jugraj81171 <98471132+Jugraj81171@users.noreply.github.com>
2024-04-25 23:48:15 -0400

    Update CountOfSubstring.java

--- src/assignments/CountOfSubstring.java
+++ src/assignments/CountOfSubstring.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class CountOfSubstring {
       public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            String s = scanner.nextLine();
+            String s = scanner.nextLine(); //jagga
             int maxLength = 0;
             int[] arr = new int[256];
 
             int start = 0;
 
             for (int i = 0; i < s.length(); i++) {
-                  char currentChar = s.charAt(i);
-                  start = Math.max(start, arr[currentChar]);
-                  maxLength = Math.max(maxLength, i - start + 1);
-                  arr[currentChar] = i + 1;
+                  char currentChar = s.charAt(i); // j, a, g
+                  start = Math.max(start, arr[currentChar]); // 0, 1, 
+                  maxLength = Math.max(maxLength, i - start + 1); // 1, 1
+                  arr[currentChar] = i + 1; // 1, 2
             }
 
             System.out.println(maxLength);
